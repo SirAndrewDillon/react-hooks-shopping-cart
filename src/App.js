0629a9ac6a0356c6750a9588b716f73dbcab9bd2
andrew dillon <majik895@gmail.com>
2019-07-23 11:00:42 -0400

    MVP Complete

--- src/App.js
+++ src/App.js
@@ -7,36 +7,43 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Contexts
+import { ProductContext, CartContext } from './contexts';
+
+// Hooks
+import { useLocalStorage } from './hooks';
+
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useLocalStorage('shoppingCart', []);
 
-	const addItem = item => {
-		setCart([...cart, item]);
-	};
+  const addItem = item => {
+    setCart([...cart, item]);
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  const removeItemById = id => {
+    setCart([...cart.filter(cartItem => id !== cartItem.id)]);
+  };
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+  return (
+    <ProductContext.Provider value={{ products, addItem }}>
+      <CartContext.Provider value={{ cart, removeItemById }}>
+        <div className='App'>
+          <Navigation cart={cart} />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-	);
+          {/* Routes */}
+          {/* <Route
+          exact
+          path='/'
+          render={() => <Products products={products} addItem={addItem} />}
+				/> */}
+          <Route exact path='/' component={Products} />
+
+          <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
+        </div>
+      </CartContext.Provider>
+    </ProductContext.Provider>
+  );
 }
 
 export default App;
